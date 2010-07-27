9b4a012f09c24504cdf3f678e0263b59d16ae9fb
Naveen Kalla <nkalla@codeaurora.org>
2010-07-27 12:20:43 -0700

    Telephony: Add subaddress to SMS fingerprint calculation.
    As per SMS specification in 3GPP2 C.S0015-B, section 4.3.1.6, if the
    Subaddress is included in a CDMA SMS message, it needs to be used for
    duplicate detection. Subaddress, which is an optional field was omitted
    while computing the SMS fingerprint. Hence it was never being used in
    duplicate detection if it was included in the SMS. Add subaddress to the
    SMS fingerprint.
    
    Change-Id: I1b69dfb16aef86e5587a467ec681af7692e27e6a
    CRs-Fixed: 235885

--- telephony/java/com/android/internal/telephony/cdma/SmsMessage.java
+++ telephony/java/com/android/internal/telephony/cdma/SmsMessage.java
@@ -30,6 +30,7 @@ import com.android.internal.telephony.SmsMessageBase;
 import com.android.internal.telephony.TelephonyProperties;
 import com.android.internal.telephony.cdma.sms.BearerData;
 import com.android.internal.telephony.cdma.sms.CdmaSmsAddress;
+import com.android.internal.telephony.cdma.sms.CdmaSmsSubaddress;
 import com.android.internal.telephony.cdma.sms.SmsEnvelope;
 import com.android.internal.telephony.cdma.sms.UserData;
 import com.android.internal.util.HexDump;
@@ -146,6 +147,7 @@ public class SmsMessage extends SmsMessageBase {
         SmsMessage msg = new SmsMessage();
         SmsEnvelope env = new SmsEnvelope();
         CdmaSmsAddress addr = new CdmaSmsAddress();
+        CdmaSmsSubaddress subaddr = new CdmaSmsSubaddress();
         byte[] data;
         byte count;
         int countInt;
@@ -188,14 +190,23 @@ public class SmsMessage extends SmsMessageBase {
 
         addr.origBytes = data;
 
-        // ignore subaddress
-        p.readInt(); //p_cur->sSubAddress.subaddressType
-        p.readInt(); //p_cur->sSubAddress.odd
-        count = p.readByte(); //p_cur->sSubAddress.number_of_digits
-        //p_cur->sSubAddress.digits[digitCount] :
-        for (int index=0; index < count; index++) {
-            p.readByte();
+        subaddr.type = p.readInt(); // p_cur->sSubAddress.subaddressType
+        subaddr.odd = p.readByte();     // p_cur->sSubAddress.odd
+        count = p.readByte();           // p_cur->sSubAddress.number_of_digits
+
+        if (count < 0) {
+            count = 0;
         }
+
+        // p_cur->sSubAddress.digits[digitCount] :
+
+        data = new byte[count];
+
+        for (int index = 0; index < count; ++index) {
+            data[index] = p.readByte();
+        }
+
+        subaddr.origBytes = data;
 
         /* currently not supported by the modem-lib:
             env.bearerReply
@@ -218,6 +229,7 @@ public class SmsMessage extends SmsMessageBase {
 
         // link the the filled objects to the SMS
         env.origAddress = addr;
+        env.origSubaddress = subaddr;
         msg.originatingAddress = addr;
         msg.mEnvelope = env;
 
@@ -847,6 +859,8 @@ public class SmsMessage extends SmsMessageBase {
         output.write(mEnvelope.teleService);
         output.write(mEnvelope.origAddress.origBytes, 0, mEnvelope.origAddress.origBytes.length);
         output.write(mEnvelope.bearerData, 0, mEnvelope.bearerData.length);
+        output.write(mEnvelope.origSubaddress.origBytes, 0,
+                mEnvelope.origSubaddress.origBytes.length);
 
         return output.toByteArray();
     }
